0db86a588377a3f8ec269ebbf54b6b59da71a506
Adriano Oliveira <oliveira.adriano@gmail.com>
2026-01-22 08:26:58 -0300

    Primeiro commit: estrutura inicial do site

---
+++ imagens.js
@@ -0,0 +1,36 @@
+const midia = {
+    "carros": {
+        "Red Bull Racing": "img/cars/2026redbullracingcarright.avif",
+        "Mercedes-AMG": "img/cars/2026mercedescarright.avif",
+        "Scuderia Ferrari": "img/cars/2026ferraricarright.avif",
+        "McLaren": "img/cars/2026mclarencarright.avif",
+        "Aston Martin": "img/cars/2026astonmartincarright.avif",
+        "Alpine": "img/cars/2026alpinecarright.avif",
+        "Kick Sauber": "img/cars/2026audicarright.avif",
+        "RB": "img/cars/2026racingbullscarright.avif",
+        "Williams": "img/cars/2026williamscarright.avif",
+        "Haas": "img/cars/2026haasf1teamcarright.avif"
+    },
+    "pilotos": {
+        "Max Verstappen": "img/drivers/max.png",
+        "Sergio Pérez": "img/drivers/max.png",
+        "Lewis Hamilton": "img/drivers/max.png",
+        "George Russell": "img/drivers/max.png",
+        "Charles Leclerc": "img/drivers/max.png",
+        "Carlos Sainz": "img/drivers/max.png",
+        "Lando Norris": "img/drivers/max.png",
+        "Oscar Piastri": "img/drivers/max.png",
+        "Fernando Alonso": "img/drivers/max.png",
+        "Lance Stroll": "img/drivers/max.png",
+        "Pierre Gasly": "img/drivers/max.png",
+        "Esteban Ocon": "img/drivers/max.png",
+        "Valtteri Bottas": "img/drivers/max.png",
+        "Zhou Guanyu": "img/drivers/max.png",
+        "Yuki Tsunoda": "img/drivers/max.png",
+        "Daniel Ricciardo": "img/drivers/max.png",
+        "Alex Albon": "img/drivers/max.png",
+        "Logan Sargeant": "img/drivers/max.png",
+        "Kevin Magnussen": "img/drivers/max.png",
+        "Nico Hulkenberg": "img/drivers/max.png"
+    }
+};
